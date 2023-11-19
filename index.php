b782172e7253358a97eb0677cee7a37080fd424a
tarrique-santos <109968671+tarrique-santos@users.noreply.github.com>
2023-11-19 14:21:54 -0300

    index.php

--- index.php
+++ index.php
@@ -1,7 +1,1 @@
-<?php
-  include 'includes/header.php';
-  // Conteúdo da página inicial
-  // include 'includes/form.php';
-  // include 'includes/footer.php';
-  // include "includes/tabela.php";
-?>
+
